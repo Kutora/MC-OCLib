f194574d1b3e7c8b958f9dc76405963b10ff0e45
Kutora <32602050+Kutora@users.noreply.github.com>
2021-06-12 00:15:48 -0700

    Create redsides.lua
    Added as Utility script and template

---
+++ usr/bin/redsides.lua
@@ -0,0 +1,13 @@
+local event = require("event")
+moduTrak=0
+  for i,j in ipairs(component.redstone.getBundledOutput()) do
+    print(sides[i])
+    for k,l in ipairs(j) do
+      print("",  colors[k], l) end
+    moduTrak = (moduTrak + 1)%3
+    if moduTrak==0 then
+      event.pull("key_down")
+      print("\n\n\n")
+    end
+  end
+moduTrak=nil
